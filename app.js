460fbfe46521c412e018c0eb90c80e76c8dbf784
Sindre Moen <sindrenm@gmail.com>
2014-12-05 12:13:24 +0100

    Serve public files from /public

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.json("Hello, World!");
-});
+app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log('Express server started on port 3000');
