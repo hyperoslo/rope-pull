dbe8f2e55df14d9e669d4392af061533857c44ae
Sindre Moen <sindrenm@gmail.com>
2014-12-05 12:59:11 +0100

    Add socket.io

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var http = require('http');
+var socketIo = require('socket.io');
 
 var app = express();
 
+var server = http.Server(app);
+var io = socketIo(server);
+
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+io.on('connection', function (socket) {
+  console.log('A user connected');
+});
+
+server.listen(3000, function () {
   console.log('Express server started on port 3000');
 });
